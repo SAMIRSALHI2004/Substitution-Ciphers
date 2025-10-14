e080aced27612473187a5e1cf77f177bfa51635b
Samir Salhi <sess92629@gmail.com>
2025-10-14 23:51:16 +0200

    Implement Caesar cipher encryption and decryption in Java

---
+++ src/CaesarCipher.java
@@ -0,0 +1,30 @@
+public class CaesarCipher {
+    static String alpha ="abcdefghijklmnopqrstuvwxyz. ";
+    public static String encryption(String message){
+        String cypherText ="";
+        for (int i = 0; i < message.length(); i++) {
+            int charPositon = alpha.indexOf(message.charAt(i));
+            int newPosition = (charPositon+3)%28;
+            char newChar = alpha.charAt(newPosition);
+            cypherText+=newChar;
+
+        }
+        return cypherText;
+    };
+    public static String dencryption(String message){
+        String plainText ="";
+        for (int i = 0; i < message.length(); i++) {
+            int charPositon = alpha.indexOf(message.charAt(i));
+            int newPosition = (charPositon-3)%28;
+            if(newPosition < 0){
+                newPosition+=28;
+            }
+            char newChar = alpha.charAt(newPosition);
+            plainText+=newChar;
+        }
+        return plainText;
+    };
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("the encription message  with caesar cipher is "+ CaesarCipher.encryption("hello world"));
+        System.out.println("the decription message  with caesar cipher is "+ CaesarCipher.dencryption("khoorczruog"));
+    }
+}
